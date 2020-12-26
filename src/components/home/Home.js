83ca30106005bb758979a4ef2685d6b58a4923a4
Cris <cristina.picatoste@gmail.com>
2020-12-26 04:10:03 +0100

    Funcionalidad random cards en la home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,10 +4,17 @@ import { HeroCard } from "../heroes/HeroCard";
 import "./Home.css";
 
 export const Home = () => {
+  let heroesRandom = [];
+
+  for (let i = 0; i < heroes.length; i++) {
+    const randomI = Math.floor(Math.random() * heroes.length);
+    heroesRandom.push(heroes[randomI]);
+  }
+
   return (
     <div className="home-wrap">
       <h1>HEROES APP</h1>
-      {heroes.map((hero) => (
+      {heroesRandom.map((hero) => (
         <HeroCard key={hero.id} {...hero} />
       ))}
     </div>
